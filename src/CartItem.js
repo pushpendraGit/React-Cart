77da7b05cad2fab941484874a699d7d2066745b1
Pushpendra Singh <pushpendra7060@gmail.com>
2020-06-11 23:14:15 +0530

    there we have added the the state in over cart item

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 class CartItem extends React.Component {
 
+    constructor(){
+        super();
+        this.state={
+
+            price:999,
+            title:'Mobile Phone',
+
+            qty:1,
+            img:''
+
+            
+        }
+    }
+
     render(){
+        const{price, title, qty} = this.state
         return(
             <div className="cart-item ">
 
@@ -11,10 +26,20 @@ class CartItem extends React.Component {
                 </div>
                 <div className='right-block'>
 
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style ={{fontSize:25}}>Rs : 999</div>
-                    <div style={{fontSize:25}}>Qty: 1</div>
-                    <div className='card-item-actions'>Phone</div>
+        <div style={{fontSize:25}}>{title}</div>
+        <div style ={{color:"#777"}}>Price : {price}</div>
+        <div style={{color:'#777'}}>Qty : {qty}</div>
+                    <div className='card-item-actions'>
+                        <img alt='increase' className='action-icons' src='https://www.flaticon.com/premium-icon/icons/svg/2956/2956929.svg'/>
+                        <img alt='decrease' className='action-icons' src='https://as2.ftcdn.net/jpg/03/51/18/85/500_F_351188596_rBQwfSx3MgDI1RhmyrJcVGFfP4lVg1VN.jpg'/>
+                        <img alt='delete' className='action-icons' src='https://www.flaticon.com/premium-icon/icons/svg/2821/2821436.svg'/>
+
+
+
+
+
+
+                    </div>
 
                 </div>
             </div>
